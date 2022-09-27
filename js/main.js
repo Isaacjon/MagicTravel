7bdf073016307420f292f66098efe620e97384fd
isaacjon <izzatazimov958@gmail.com>
2022-09-27 11:51:59 +0500

    worked on navbar's toggle code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 let burgerBtn = document.querySelector(".menu-burger");
 let nav = document.querySelector(".nav");
-console.log(burgerBtn);
 
-burgerBtn.addEventListener("click", () => {
+const toggleNav = () => {
   nav.classList.toggle("active");
 
   nav.classList.contains("active")
     ? burgerBtn.classList.add("fa-xmark")
     : burgerBtn.classList.remove("fa-xmark");
+};
+
+burgerBtn.addEventListener("click", toggleNav);
+
+window.addEventListener("click", (e) => {
+  burgerBtn.classList.contains("fa-xmark") &&
+    e.target != burgerBtn &&
+    e.target != nav &&
+    toggleNav();
 });
